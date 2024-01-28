ea879fb7708d2fbc9027a0fdfc900aadc234bb35
Allen Tsai <a0962071938@gmail.com>
2024-01-28 13:59:13 +0800

    使用範例改寫echo bot-38

--- command_func.py
+++ command_func.py
@@ -1,3 +1,4 @@
+from init import *
 import html
 from telegram import Update
 from telegram.ext import (
@@ -7,8 +8,8 @@ from telegram.ext import (
 
 async def start(update: Update, context: CallbackContext) -> None:
     """Display a message with instructions on how to use this bot."""
-    server_url = context.bot_data.get["SEVER_URL"]
-    print("server_url : ", server_url)
+    server_url = SEVER_URL
+    print(context)
     payload_url = html.escape(f"{server_url}/submitpayload?user_id=<your user id>&payload=<payload>")
     text = (
         f"To check if the bot is still running, call <code>{server_url}/healthcheck</code>.\n\n"
