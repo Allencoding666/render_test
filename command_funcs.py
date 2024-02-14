ae4c8d019e5fde921adfe015e62b52a73c111de8
Allen Tsai <a0962071938@gmail.com>
2024-02-14 16:45:12 +0800

    修正天氣預報功能，只撈取近8小時的天氣預報

--- command_funcs.py
+++ command_funcs.py
@@ -4,17 +4,15 @@ from telegram import Update
 from telegram.ext import (
     CallbackContext,
 )
-import requests
 import aiohttp
+from urllib.parse import quote
+from datetime import datetime, timedelta
+import pytz
 
 
 async def start(update: Update, context: CallbackContext) -> None:
     """Display a message with instructions on how to use this bot."""
     server_url = SEVER_URL
-    print("update : ", update)
-    print("update.message : ", update.message)
-    print("update.api_kwargs : ", update.api_kwargs)
-    print(context.args[0])
 
     payload_url = html.escape(f"{server_url}/submitpayload?user_id=<your user id>&payload=<payload>")
     text = (
@@ -25,25 +23,33 @@ async def start(update: Update, context: CallbackContext) -> None:
 
 
 async def weather(update: Update, context: CallbackContext) -> None:
-    """取得台中市天氣預報"""
-    url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-073?Authorization=CWA-55E41616-4DFC-494F-8D12-2121983E8639&limit=1&locationName=%E8%A5%BF%E5%B1%AF%E5%8D%80&elementName=WeatherDescription"
+    """取得台中市-西屯區天氣預報"""
+    current_time = datetime.now(pytz.timezone('Asia/Taipei'))
+    eight_hours_later_time = current_time + timedelta(hours=8)
+    encoded_current_time = quote(current_time.strftime("%Y-%m-%dT%H:%M:%S"))
+    encoded_eight_hours_later_time = quote(eight_hours_later_time.strftime("%Y-%m-%dT%H:%M:%S"))
+
+    url = (f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-073"
+           f"?Authorization={WEATHER_AUTH_CODE}"
+           f"&locationName=%E8%A5%BF%E5%B1%AF%E5%8D%80"
+           f"&elementName=WeatherDescription"
+           f"&timeFrom={encoded_current_time}"
+           f"&timeTo={encoded_eight_hours_later_time}")
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as rsp:
             rsp_json = await rsp.json()
 
-    # rsp = requests.get(url)
-    # rsp_json = rsp.json()
     city = rsp_json["records"]["locations"][0]["locationsName"]
     district = rsp_json["records"]["locations"][0]["location"][0]["locationName"]
     description = rsp_json["records"]["locations"][0]["location"][0]["weatherElement"][0]["description"]
 
     all_data = rsp_json["records"]["locations"][0]["location"][0]["weatherElement"][0]["time"]
-    result = f"{city} : {district}\n{description}\n\n"
+    result = f"{city} : {district}\n{description}\n當前時間 : {current_time.strftime("%Y-%m-%d %H:%M:%S")}\n\n"
     for data in all_data:
         start_time = data["startTime"]
         end_time = data["endTime"]
         value = data["elementValue"][0]["value"]
-        result += f"{start_time} ~ {end_time}\n - 天氣預報 : {value}\n"
+        result += f"{start_time} ~ {end_time}\n - 天氣預報 : {value}\n\n"
 
     await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=result)
--- setting.py
+++ setting.py
@@ -1,8 +1,7 @@
 import os
 
-# Define configuration constants
 SEVER_URL = os.environ.get("SEVER_URL")
 ADMIN_CHAT_ID = os.environ.get("ADMIN_CHAT_ID")
 PORT = 5000
 BOT_TOKEN = os.getenv("TOKEN")
-
+WEATHER_AUTH_CODE = os.getenv("WEATHER_AUTH_CODE")
